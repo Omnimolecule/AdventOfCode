16094e2a7011b850c1a5e640f41e43453cb7cd92
Jasmin Frei <jasmin.frei@hotmail.com>
2017-12-10 14:27:49 +0100

    solution day 10 part 2

--- src/main/kotlin/ch/omni/aoc/year2017/Day10.kt
+++ src/main/kotlin/ch/omni/aoc/year2017/Day10.kt
@@ -3,30 +3,72 @@ package ch.omni.aoc.year2017
 import readFile
 
 object Day10 {
-    val hash = listOf(0..255).flatten().toMutableList()
-    val inputLength = readFile(10).split(",").map{it.toInt()}
+
+    val input = readFile(10)
     fun start() {
+        part1()
+        part2()
+    }
+
+    fun part1() {
+        val hash = listOf(0..255).flatten().toMutableList()
+        val inputLength = input.split(",").map{it.toInt()}
+        doOneRound(hash, inputLength, 0, 0)
+        println(hash.get(0) * hash.get(1))
+    }
+
+    fun part2(){
+        val hash = listOf(0..255).flatten().toMutableList()
+        val inputLength = input.toCharArray().map{it.toInt()}.toMutableList()
+        inputLength.addAll(mutableListOf(17,31,73,47,23))
         var idx = 0
         var skipSize = 0
+        for (i in 0..63){
+            val result = doOneRound(hash, inputLength, idx, skipSize)
+            idx = result.first
+            skipSize = result.second
+        }
+        val denseHash = mutableListOf<Int>()
+        var index = 0
+        for (i in 0..15){
+            var res = 0
+            for (j in 0..15) {
+                res = res xor hash.get(index)
+                index++
+            }
+            denseHash.add(res)
+        }
+        println(denseHash.map{
+            if (it.toString(16).length < 2) {
+                "0" + it.toString(16)
+            } else {
+                it.toString(16)
+            }
+        }.joinToString(""))
+    }
+
+    private fun doOneRound(hash: MutableList<Int>, inputLength: List<Int>, idx: Int, skipSize: Int):Pair<Int, Int> {
+        var idx1 = idx
+        var skipSize1 = skipSize
         for (length in inputLength) {
             val area = mutableListOf<Pair<Int, Int>>()
 
-            for (i in idx..(idx + length)-1) {
-                if (i < hash.size){
+            for (i in idx1..(idx1 + length) - 1) {
+                if (i < hash.size) {
                     area.add(Pair(i, hash.get(i)))
                 } else {
-                    area.add(Pair(i%hash.size, hash.get(i%hash.size)))
+                    area.add(Pair(i % hash.size, hash.get(i % hash.size)))
                 }
             }
             val reversedArea = area.reversed()
             reversedArea.forEachIndexed { index, value ->
                 hash.set(area.get(index).first, value.second)
             }
-            idx += (skipSize + length)
-            idx = idx % hash.size
-            skipSize++
+            idx1 += (skipSize1 + length)
+            idx1 = idx1 % hash.size
+            skipSize1++
         }
-        println(hash.get(0) * hash.get(1))
+        return Pair(idx1, skipSize1)
     }
 }
 
