46e9d886cbcdf5f583eca0b76a29ba424fe87164
Jasmin Frei <jasmin.frei@zkb.ch>
2017-12-11 12:34:52 +0100

    Part1 and Part2 with a little help from the internet http://keekerdc.com/2011/03/hexagon-grids-coordinate-systems-and-distance-calculations/

--- src/main/kotlin/ch/omni/aoc/year2017/Day11.kt
+++ src/main/kotlin/ch/omni/aoc/year2017/Day11.kt
@@ -2,22 +2,42 @@ package ch.omni.aoc.year2017
 
 import readFile
 
+/**
+ * Solution based on this link:
+ * http://keekerdc.com/2011/03/hexagon-grids-coordinate-systems-and-distance-calculations/
+ */
 object Day11 {
 
     fun start(){
-        val input = "se,sw,se,sw,sw" //readFile(11)
-        val blub = input.split(",").groupingBy { it }.eachCount()
-        println(blub)
-        val hor = Math.abs((blub["w"]?:0).minus(blub["e"]?:0))
-        val ver = Math.abs((blub["n"]?:0).minus(blub["s"]?:0))
-        val diagL = Math.abs((blub["nw"]?:0).minus(blub["se"]?:0))
-        val diagR = Math.abs((blub["sw"]?:0).minus(blub["ne"]?:0))
-
-        println(hor)
-        println(ver)
-        println(diagL)
-        println(diagR)
-        println(diagL+diagR-ver-hor)
+        val input = readFile(11)
+        val directions = input.split(",")
+        var y = 0
+        var x = 0
+        var max = 0
+        for (direction in directions){
+            when(direction) {
+                "n" -> y++
+                "ne" -> {
+                    y++
+                    x--
+                }
+                "se" -> x--
+                "s" -> y--
+                "sw" -> {
+                    y--
+                    x++
+                }
+                "nw" -> x++
+            }
+            val z = 0-x-y
+            val tempMax = (listOf(Math.abs(x), Math.abs(y), Math.abs(z)).max())?:0
+            if (tempMax > max) {
+                max = tempMax
+            }
+        }
+        val z = 0-x-y
+        println((listOf(Math.abs(x), Math.abs(y), Math.abs(z)).max()))
+        println(max)
     }
 }
 
