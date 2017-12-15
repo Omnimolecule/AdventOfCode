bf7ee3328b3007f9a6d4c9489fbdb32c660e4f34
Jasmin Frei <jasmin.frei@hotmail.com>
2017-12-15 09:21:09 +0100

    day 15 part 2

--- src/main/kotlin/ch/omni/aoc/year2017/Day15.kt
+++ src/main/kotlin/ch/omni/aoc/year2017/Day15.kt
@@ -3,15 +3,52 @@ package ch.omni.aoc.year2017
 import java.math.BigDecimal
 
 object Day15 {
+    val originalA = BigDecimal(116)
+    val originalB = BigDecimal(299)
+    val multiplierA = BigDecimal(16807)
+    val multiplierB = BigDecimal(48271)
+    val divisor = BigDecimal(2147483647)
+
 
     fun start(){
-        var inputA = BigDecimal(116)
-        var inputB = BigDecimal(299)
+        part1()
+        part2()
+    }
+
+    fun part1() {
+        var inputA = originalA
+        var inputB = originalB
         var count = 0
 
         for (i in 0..39999999) {
-            inputA = inputA.multiply(BigDecimal(16807)).rem(BigDecimal(2147483647))
-            inputB = inputB.multiply(BigDecimal(48271)).rem(BigDecimal(2147483647))
+            inputA = inputA.multiply(multiplierA).rem(divisor)
+            inputB = inputB.multiply(multiplierB).rem(divisor)
+
+            val aString = "0000000000000000" + inputA.toInt().toString(2)
+            val bString = "0000000000000000" + inputB.toInt().toString(2)
+
+            if (aString.substring(aString.length-16).equals(bString.substring(bString.length-16))) {
+                count++
+            }
+        }
+        println(count)
+    }
+
+    fun part2() {
+        var inputA = originalA
+        var inputB = originalB
+        var count = 0
+
+        for (i in 0..4999999) {
+            inputA = inputA.multiply(multiplierA).rem(divisor)
+            while (inputA.rem(BigDecimal(4)).toInt() != 0) {
+                inputA = inputA.multiply(multiplierA).rem(divisor)
+            }
+
+            inputB = inputB.multiply(multiplierB).rem(divisor)
+            while(inputB.rem(BigDecimal(8)).toInt() != 0) {
+                inputB = inputB.multiply(multiplierB).rem(divisor)
+            }
 
             val aString = "0000000000000000" + inputA.toInt().toString(2)
             val bString = "0000000000000000" + inputB.toInt().toString(2)
