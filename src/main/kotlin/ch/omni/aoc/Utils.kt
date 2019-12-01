876ad0a1d1f69ac11a15946820d409bed2718e2a
Jasmin Frei <jasmin.frei@hotmail.com>
2019-12-01 16:00:14 +0100

    added the possibility to add and read inputs for multiple years

--- src/main/kotlin/ch/omni/aoc/Utils.kt
+++ src/main/kotlin/ch/omni/aoc/Utils.kt
@@ -7,3 +7,11 @@ fun readFile(day:Int): String {
     stream = stream.replace("\t", " ")
     return stream
 }
+
+fun readFile(year:Int, day:Int): String {
+    val inputStream: InputStream = ClassLoader.getSystemResourceAsStream("$year/input$day.txt")
+    var stream =  inputStream.bufferedReader().use { it.readText() }
+    stream = stream.replace("\r", "")
+    stream = stream.replace("\t", " ")
+    return stream
+}
