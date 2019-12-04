a94a93f1219871a5436d664844d0d510e9b863c7
Jasmin Frei <jasmin.frei@hotmail.com>
2019-12-04 06:41:11 +0100

    Day 4 part 2

--- src/main/kotlin/ch/omni/aoc/year2019/Day04.kt
+++ src/main/kotlin/ch/omni/aoc/year2019/Day04.kt
@@ -3,6 +3,7 @@ package ch.omni.aoc.year2019
 object Day04 {
     fun start() {
         part1(146810, 612564)
+        part2(146810, 612564)
     }
 
     fun part1(from: Int, to: Int) {
@@ -32,15 +33,53 @@ object Day04 {
         return doubleFound
     }
 
+    fun checkValid2(pw: Int):Boolean {
+        val stringPw = pw.toString()
+        var lastOne = '0'
+        var sameCount = 0
+        var doubleFound = false
+        for (char in stringPw){
+            if (char.toInt() >= lastOne.toInt()){
+                if (char.equals(lastOne)){
+                    sameCount++
+                } else {
+                    if (sameCount == 1){
+                        doubleFound = true
+                    }
+                    sameCount = 0
+                }
+            } else {
+                return false
+            }
+            lastOne = char
+        }
+        if (sameCount == 1){
+            doubleFound = true
+        }
+        return doubleFound
+    }
 
-    fun part2() {
 
+    fun part2(from: Int, to: Int) {
+        val result: MutableList<Int> = mutableListOf()
+        for (pw in from .. to) {
+            if (checkValid2(pw)) {
+                result.add(pw)
+            }
+        }
+        println(result.size)
     }
 
     fun test1() {
         println(checkValid(111111))
         println(checkValid(223450))
         println(checkValid(123789))
+    }
+
+    fun test2() {
+        println(checkValid2(112233))
+        println(checkValid2(123444))
+        println(checkValid2(111122))
     }
 }
 
