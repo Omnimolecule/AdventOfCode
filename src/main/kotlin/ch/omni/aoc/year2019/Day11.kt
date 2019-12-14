1c1b7aa487574c19ac95e05c75ceb382dc9ff6af
Jasmin Frei <jasmin.frei@hotmail.com>
2019-12-14 17:22:52 +0100

    Day 11 part 1

---
+++ src/main/kotlin/ch/omni/aoc/year2019/Day11.kt
@@ -0,0 +1,96 @@
+package ch.omni.aoc.year2019
+
+import readFile
+
+object Day11 {
+
+    fun start() {
+        val input = readFile(2019, 11).split(",").map { it.toLong() }
+        part1(input)
+    }
+
+    fun part1(input: List<Long>) {
+        val robo = IntcodeComputerV4(input, manualMode = false, returnOnOutput = true)
+        val map: MutableMap<Pair<Int, Int>, Long> = mutableMapOf()
+        var x = 0
+        var y = 0
+        var orientation = 0
+        while (!robo.isDone) {
+            val value1 = robo.calculate(listOf(map.getOrDefault(Pair(x, y), 0L))).second.last()
+            if (robo.isDone) break
+            val value2 = robo.calculate().second.last()
+            if (value1 == 0L) {
+                map.put(Pair(x, y), 0L)
+            } else {
+                map.put(Pair(x, y), 1L)
+            }
+            when (value2) {
+                0L -> {
+                    when (orientation) {
+                        0 -> {
+                            orientation += 90
+                            x += 1
+                        }
+                        90 -> {
+                            orientation += 90
+                            y -= 1
+                        }
+                        180 -> {
+                            orientation += 90
+                            x -= 1
+                        }
+                        270 -> {
+                            orientation = 0
+                            y += 1
+                        }
+                    }
+                }
+                1L -> {
+                    when (orientation) {
+                        0 -> {
+                            orientation = 270
+                            x -= 1
+                        }
+                        90 -> {
+                            orientation -= 90
+                            y += 1
+                        }
+                        180 -> {
+                            orientation -= 90
+                            x += 1
+                        }
+                        270 -> {
+                            orientation -= 90
+                            y -= 1
+                        }
+                    }
+                }
+            }
+        }
+        println(map.size)
+        printOutput(map)
+    }
+
+    fun printOutput(input: Map<Pair<Int, Int>, Long>) {
+        val inputList = input.toList()
+        val minX = inputList.minBy { it.first.first }!!.first.first
+        val maxX = inputList.maxBy { it.first.first }!!.first.first
+        val minY = inputList.minBy { it.first.second }!!.first.second
+        val maxY = inputList.maxBy { it.first.second }!!.first.second
+        for (x in minX until maxX) {
+            for (y in minY until maxY) {
+                val paint = input.getOrDefault(Pair(x, y), 0L)
+                if (paint == 0L) {
+                    print("\u2591\u2591")
+                } else {
+                    print("  ")
+                }
+            }
+            println()
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    Day11.start()
+}
